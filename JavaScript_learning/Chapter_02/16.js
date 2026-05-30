360db39ef6e01361b1005e86880864968e574248
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-05-30 18:29:04 +0530

    Initial files

---
+++ JavaScript_learning/Chapter_02/16.js
@@ -0,0 +1,6 @@
+//Program 16: Reverse a string without using built-in methods.
+//for browser
+//let str = prompt("Enter the word");
+let str = "Abhi";
+const reversedWord = str.split("").reverse().join("");
+console.log(reversedWord);
