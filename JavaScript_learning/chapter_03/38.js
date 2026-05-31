6f8672c118778abf4c35320df3b4716302eb627b
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-05-31 15:35:16 +0530

    containt add

--- JavaScript_learning/chapter_03/38.js
+++ JavaScript_learning/chapter_03/38.js
@@ -1,1 +1,11 @@
-
+// Program 38: Reverse an array in-place without creating a new array.
+let arr = [1, 2, 8, 4, 7, 9, 5];
+console.log(arr);
+let temp;
+let j = arr.length - 1;
+for (let i = 0; i <= arr.length / 2; i++, j--) {
+  temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+console.log(arr);
