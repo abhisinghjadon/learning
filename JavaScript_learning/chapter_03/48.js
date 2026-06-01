8061de2632f19fdfdb7ae2de1b4ecf7f5d0c43ac
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-06-01 17:49:29 +0530

    Add program to double numbers in an array

---
+++ JavaScript_learning/chapter_03/48.js
@@ -0,0 +1,5 @@
+// Program 48: Double every number in an array using .map().
+let arr = [1, 5, 9, 4, 10, 8, 12];
+// .map() loops through the original array element by element.
+let doubledNumbers = arr.map((num) => num * 2);
+console.log(doubledNumbers);
