cd3decbafbb3b49d104a176b05adbc5391c1ae8a
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-06-01 17:47:02 +0530

    Fix filter to extract odd numbers from array

--- JavaScript_learning/chapter_03/47.js
+++ JavaScript_learning/chapter_03/47.js
@@ -1,6 +1,6 @@
 // Program 47: Filter out all odd numbers from an array using .filter().
 let arr = [1, 5, 9, 4, 10, 8, 12];
 
-let evenNumbers = arr.filter((num) => num % 2 != 0);
+let odd = arr.filter((num) => num % 2 != 0);
 
-console.log(evenNumbers); 
+console.log(odd); 
