737d87c726429b90d8c1142f67db99bd009c04da
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-06-01 21:39:15 +0530

    Implement 2D matrix array and print elements
    This program creates a 2D matrix array and prints its elements in a formatted manner.

---
+++ JavaScript_learning/chapter_03/54.js
@@ -0,0 +1,15 @@
+//Program 54: Create a 2D matrix array and print its elements
+l
+
+let str="";
+let arr=[[1,2,7],[3,4,7],[5,6,7]]
+for(let i=0; i<3;i++ )
+{
+   for (let j=0;j<3;j++)
+   {
+      str+=arr[i][j]
+   }
+   str+="\n"
+  
+}
+ console.log(str)
