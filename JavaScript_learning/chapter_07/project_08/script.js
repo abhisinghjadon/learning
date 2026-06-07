45fe93b25c2c3d20d7dc292cfd9364ba5ae4eea6
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-06-07 15:05:00 +0530

    Create script.js

---
+++ JavaScript_learning/chapter_07/project_08/script.js
@@ -0,0 +1,24 @@
+const image = document.getElementById("myImage");
+const source = ["10101.jpg", "10102.jpg", "10103.jpg"];
+let current = 0;
+function change(a) {
+  if (a == "next") {
+    current++;
+  } else {
+    if (current == 0) {
+      current = 2;
+    } else current--;
+  }
+
+  if (current >= source.length) {
+    current = 0;
+  }
+
+  image.src = source[current];
+}
+
+//auto run like slideshow  after 1 sec change Image
+
+// setInterval(() => {
+//   change("next");
+// }, 1000);
