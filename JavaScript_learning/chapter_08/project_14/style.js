31a70d29e5e24538072089cd1995ceb5acfb3c9f
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-06-08 16:40:10 +0530

    Add CSS styles for layout and table formatting

---
+++ JavaScript_learning/chapter_08/project_14/style.js
@@ -0,0 +1,56 @@
+.Container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+  max-width: 500px;
+  margin: 5% auto;
+  font-size: 7rem;
+}
+.buttonconta {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  width: 100%;
+  max-width: 500px;
+  margin: auto;
+  font-size: 1rem;
+}
+table {
+  width: 100%;
+  max-width: 800px;
+  margin: 20px auto;
+  border-collapse: collapse;
+  font-family: sans-serif;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  font-size: 1.5rem;
+}
+
+thead tr {
+  background-color: #30427c;
+  color: #ffffff;
+  text-align: center;
+}
+
+th,
+td {
+  padding: 12px 15px;
+  border: 1px solid #dddddd;
+  text-align: center;
+}
+
+tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
