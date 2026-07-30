7348507124015c8f40f863c969280f6e5f63524e
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-07-30 15:26:06 +0530

    Add program to sum two numbers

---
+++ Js-july/ch01/10.js
@@ -0,0 +1,6 @@
+//Program 2: Take two numbers and print their sum.
+const prompt = require("prompt-sync")();
+const num1 = Number(prompt("Enter the First Number : "));
+const num2 = Number(prompt("Enter the Second Number : "));
+
+console.log(num1 + num2);
