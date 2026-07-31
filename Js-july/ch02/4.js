1b8be0c483b0fad03ab9e9380bdae77595f92f56
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-07-31 16:33:31 +0530

    Implement word capitalization in a sentence
    Added a program to capitalize the first letter of each word in a sentence.

--- Js-july/ch02/4.js
+++ Js-july/ch02/4.js
@@ -1,1 +1,10 @@
 
+// Program 20: Convert the first letter of each word in a sentence to uppercase.
+
+const str = "This is my first sentence";
+let temp = str.split(" ");
+let val;
+for (let i = 0; i < temp.length; i++) {
+  temp[i] = temp[i][0].toUpperCase() + temp[i].slice(1);
+}
+console.log(temp.join(" "));
