af5b4c06322384c344555425517483d58065f12c
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-07-31 16:36:34 +0530

    Add program to find the longest word in a sentence

--- Js-july/ch02/7.js
+++ Js-july/ch02/7.js
@@ -1,1 +1,11 @@
 
+// Program 32: Find the longest word in a sentence.
+const str = "hello ham to badiya hai aap sunao";
+let words = str.split(" ");
+let largestWord = words[0];
+for (let i = 0; i < words.length; i++) {
+  if (largestWord.length < words[i].length) {
+    largestWord = words[i];
+  }
+}
+console.log(`${largestWord} and its length is ${largestWord.length}`);
