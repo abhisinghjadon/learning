a29549ca69563ac6d1278855f7cd084985c114c1
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-07-31 16:36:14 +0530

    Implement string to slug-case conversion
    This program converts a string to slug-case by replacing spaces with hyphens and transforming characters to lowercase.

--- Js-july/ch02/6.js
+++ Js-july/ch02/6.js
@@ -1,1 +1,19 @@
 
+// Program 31: Convert a string to slug-case (e.g., "Hello World" -> "hello-world").
+
+const str = "Hello world ";
+let temp = str.slice(" ");
+let res = "";
+let val;
+for (let i = 0; i < temp.length; i++) {
+  val = temp[i].toLowerCase();
+  if (val == " ") {
+    if (i == temp.length - 1) {
+      continue;
+    }
+    res += "-";
+    continue;
+  }
+  res += val;
+}
+console.log(res);
