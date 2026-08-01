7172dec1f99adc2ae3a6e752fd09798ada47dd2d
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-01 16:01:46 +0530

    Add in-place array reversal program
    Implemented an in-place array reversal algorithm.

--- Js-july/ch03/2.js
+++ Js-july/ch03/2.js
@@ -1,1 +1,12 @@
 
+// Program 38: Reverse an array in-place without creating a new array.
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let temp;
+let length = data.length;
+for (let i = 0; i < length / 2; i++) {
+  temp = data[length - 1 - i];
+  data[length - 1 - i] = data[i];
+  data[i] = temp;
+}
+console.log(data);
