dd13b2812279fb158d9648a805b90b36f5cb357d
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-01 16:01:30 +0530

    Find largest and smallest elements in an array

--- Js-july/ch03/1.js
+++ Js-july/ch03/1.js
@@ -1,1 +1,17 @@
 
+// Program 36: Find the largest and smallest element in an array (bina Math.min/Math.max ke, single loop se).
+
+const data = [1, 8, 56, -7, 10, 789, 41, 12, 42];
+let val,
+  min,
+  max = data[0];
+min = max;
+
+for (let i = 0; i < data.length; i++) {
+  val = data[i];
+  if (val > max) max = val;
+  if (val < min) min = val;
+}
+
+console.log(`Minimun value ${min}`);
+console.log(`Maximun value ${max}`);
