f1e6463d2bf35fcfee831109aaf0f4575b277d02
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-02 16:26:02 +0530

    Create a basic Car object with properties

---
+++ Js-july/ch04/1.js
@@ -0,0 +1,8 @@
+// Program 61: Create a basic object representing a Car with properties like brand, model, and year.
+const car = {
+  name: "thar",
+  brand: "mahindra",
+  model: "Petrol",
+  year: "2026",
+};
+console.log(car);
