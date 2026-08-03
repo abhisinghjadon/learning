0bffa64c0128a3d0363d658802ee245ac630ee6e
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-03 16:32:50 +0530

    Count properties in car object using Object.keys
    Updated the method to count properties in the car object using Object.keys.

--- Js-july/ch04/8.js
+++ Js-july/ch04/8.js
@@ -1,1 +1,18 @@
 
+// Program 68: Count the number of properties present in an object.
+const car = {
+  name: "thar",
+  brand: "mahindra",
+  model: "Petrol",
+  year: "2026",
+};
+let property = 0;
+
+// for (const key in car) {
+//   if (!Object.hasOwn(car, key)) continue;
+
+//   property++;
+// }
+//modern
+const propertyCount = Object.keys(car).length;
+console.log(property);
