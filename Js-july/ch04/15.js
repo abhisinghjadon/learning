e6981867c8b55f7308f325f13f1ed19ed5973f88
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-03 16:35:28 +0530

    Add product array and sort by price

--- Js-july/ch04/15.js
+++ Js-july/ch04/15.js
@@ -1,1 +1,15 @@
+// Program 75: Create an array of objects representing products and sort them by price.
+const products = [
+  { id: 1, name: "Laptop", price: 55000 },
+  { id: 2, name: "Mobile", price: 15000 },
+  { id: 3, name: "Headphones", price: 2500 },
+  { id: 4, name: "Smartwatch", price: 4000 },
+];
 
+console.log("--- Befaore Sorting Products ---");
+console.log(products);
+
+products.sort((a, b) => a.price - b.price);
+
+console.log("\n--- After Sorting Products (Low to High Price) ---");
+console.log(products);
