36212e793a68023a9eda3c2cb49520b4e2665d03
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-04 20:55:37 +0530

    Implement callback functions for user login/logout
    Added callback functions for login and logout processes.

--- Js-july/ch05/2.js
+++ Js-july/ch05/2.js
@@ -1,1 +1,18 @@
 
+// Program 77: Write a function that accepts a callback function and executes it.
+
+function process(name, callback) {
+  console.log(`Processing data for: ${name}...`);
+  return callback(name);
+}
+
+function login(name) {
+  console.log(`welcome Back ${name} You Login Succeefully!......`);
+}
+
+function logout(name) {
+  console.log(`See you soon... ${name} You logout Succeefully!......`);
+}
+
+process("Abhishek", login);
+process("Abhishek", logout);
