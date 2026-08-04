fe9ad9541d6de6b57b61ee2fc307d35601557ecd
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-04 20:58:21 +0530

    Implement sum function using Rest Parameters

--- Js-july/ch05/5.js
+++ Js-july/ch05/5.js
@@ -1,1 +1,11 @@
 
+// Program 80: Create a function that uses Rest Parameters to sum any number of arguments.
+function sum(...num) {
+  let total = 0;
+  num.forEach((element) => {
+    total += element;
+  });
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
