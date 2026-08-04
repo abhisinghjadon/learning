1b076dec851c208d812f671d714261c45682b7ce
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-04 20:54:49 +0530

    Add function declaration, expression, and arrow function

--- Js-july/ch05/1.js
+++ Js-july/ch05/1.js
@@ -1,1 +1,24 @@
 
+// Program 76: Write a basic function declaration, function expression, and arrow function.
+
+//function declaration
+function callTax(tax, amount) {
+  return amount + amount * (tax / 100);
+}
+
+//function expression
+
+const etax = function callTax(tax, amount) {
+  return amount + amount * (tax / 100);
+};
+
+//arrow functio
+const taxarrow = (tax, amount) => amount + amount * (tax / 100);
+
+let amt = 1000;
+let tax = 10;
+
+console.log(`tax from a simple function ` + callTax(tax, amt));
+console.log(`tax from a Expression function ` + etax(tax, amt));
+console.log(`tax from a simple function ` + taxarrow(tax, amt));
+console.log();
