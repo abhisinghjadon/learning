faf8405ebf4aecd5654e0dace483dab6c6027ccb
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-04 20:57:16 +0530

    Add closure to remember counter value
    Implemented a closure that maintains a counter value.

--- Js-july/ch05/4.js
+++ Js-july/ch05/4.js
@@ -1,1 +1,15 @@
 
+// Program 79: Implement a basic Closure that remembers a counter value.
+
+function create() {
+  let counter = 0;
+  return function icrement() {
+    return ++counter;
+  };
+}
+
+const coun = create();
+console.log(coun());
+console.log(coun());
+console.log(coun());
+console.log(coun());
