c1009feb3612db75fbe2ff26bb1701e74d93bd6f
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-05 08:09:46 +0530

    Add Promise rejection handling example
    Implemented a Promise that rejects based on a condition and handles errors with .catch().

---
+++ Js-july/ch06/4.js
@@ -0,0 +1,21 @@
+// Program 89: Create a Promise that rejects if a condition is not met, and handle it via .catch().
+// Program 88: Create a basic Promise that resolves with a success message.
+const prom = new Promise((resolve, reject) => {
+  console.log("Starting to Connect....");
+  let promresult = false;
+  result = "Resolve";
+  fail = "Reject from some Reason";
+  setTimeout(() => {
+    if (promresult) {
+      resolve(result);
+    } else reject(fail);
+  }, 2000);
+});
+
+prom
+  .then((resolve) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
