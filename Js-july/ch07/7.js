aa805c4708bdee0720d38886bf5b08558df13ae4
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-07 07:31:31 +0530

    Add async function to fetch data with error handling

---
+++ Js-july/ch07/7.js
@@ -0,0 +1,21 @@
+// Program 92: Handle errors inside an async function using a try/catch block.
+// Program 91: Convert a promise-based chain into modern async/await syntax.
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    console.log("Finding the Data .....");
+
+    setTimeout(() => {
+      let data = "Data Recevied";
+      resolve(data);
+    }, 2500);
+  });
+}
+
+(async () => {
+  try {
+    const data = await fetchData();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+})();
