85bf2b5138c81ecf0fca850e6c422c4d08732417
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-08 18:27:35 +0530

    Add element selection example with query selectors
    Implemented element selection using querySelector and querySelectorAll.

---
+++ DOM_JS/Ch01/2.js
@@ -0,0 +1,11 @@
+/*
+Program 102: Query Selectors & NodeLists
+Task: Select a single element using document.querySelector() and multiple elements using document.querySelectorAll(). Log the resulting NodeList to the console.
+Syntax Focus: document.querySelector(), document.querySelectorAll(), NodeList.forEach().
+ */
+
+const tab = document.querySelector("#list");
+console.log(tab);
+
+const tab = document.querySelectorAll(".listData");
+console.log(tab);
