eacc39a10aeb42ef422d4f19eecd3dda5da4e24c
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-15 10:25:13 +0530

    Add custom context menu on right-click
    Implemented a custom context menu that appears on right-click and hides on click.

--- DOM_JS/Ch03/10.js
+++ DOM_JS/Ch03/10.js
@@ -1,1 +1,18 @@
+/**
+ * 
+Program 130: Custom Context Menu (Right-Click Interception)
+Task: Prevent the default browser right-click menu using e.preventDefault() and display a custom custom <div> menu at the mouse coordinates.
+Syntax Focus: contextmenu event, e.preventDefault(), absolute positioning.
+ */
+const menu = document.getElementById("menu");
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  //console.log("hello");
+  menu.style.left = e.pageX + "px";
+  menu.style.top = e.pageY + "px";
+  menu.style.display = "block";
+});
 
+document.addEventListener("click", () => {
+  menu.style.display = "none";
+});
