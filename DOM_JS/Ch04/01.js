84d9b769b744b1f89d5efff887036e3b354751b9
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-15 10:28:14 +0530

    Add dynamic list item creation functionality
    Implement dynamic creation of list items on button click.

--- DOM_JS/Ch04/01.js
+++ DOM_JS/Ch04/01.js
@@ -1,1 +1,16 @@
+/**
+ * rogram 131: Dynamic Element Creation
+Task: Create a new <li> element using document.createElement(), assign text content to it, and append it to an existing <ul> using appendChild().
+Syntax Focus: document.createElement(), parent.appendChild().
+ */
 
+const list = document.getElementById("list");
+let value = 5;
+
+document.getElementById("btn").addEventListener("click", () => {
+  const li = document.createElement("li");
+  let str = "List " + value;
+  value++;
+  li.innerText = str;
+  list.appendChild(li);
+});
