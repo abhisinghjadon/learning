3ad27d4d9cb0538b664f536839bceb60a81cf0ae
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-16 08:34:47 +0530

    Update 4.js

--- DOM_JS/Ch04/4.js
+++ DOM_JS/Ch04/4.js
@@ -1,1 +1,15 @@
 
+/**
+ * Program 134: Parent & Child Node Traversal
+Task: Select a child element, traverse up to its parent using parentElement, and then highlight all child elements of that parent using children.
+Syntax Focus: element.parentElement, element.children.
+ */
+
+const id = document.getElementById("cl1");
+const parent = id.parentElement;
+//console.log(parent);
+const child = [...parent.children];
+//console.log(child);
+child.forEach((element) => {
+  element.style.backgroundColor = "red";
+});
