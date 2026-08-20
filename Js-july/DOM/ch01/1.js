bfe2502074472a2874d1427f8e88eb358a8cbbaf
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-20 21:58:24 +0530

    Remove introductory comments from 1.js
    Removed comments explaining the program's purpose.

--- Js-july/DOM/ch01/1.js
+++ Js-july/DOM/ch01/1.js
@@ -1,10 +1,5 @@
 
-/*
-Program 101: Basic Selection & Text Content Modification
-Task: Select a heading element using document.getElementById() and change its text content dynamically.
-Syntax Focus: document.getElementById(), element.textContent.
 
-*/
 
 let dynBox = document.getElementById("para");
 let inp = document.getElementById("input");
