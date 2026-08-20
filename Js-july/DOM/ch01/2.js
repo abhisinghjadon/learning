1f7962a075ac2960676210b1fcb268632e9430c3
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-20 21:57:07 +0530

    Implement dynamic text content modification
    Add functionality to change text content dynamically based on input.

--- Js-july/DOM/ch01/2.js
+++ Js-july/DOM/ch01/2.js
@@ -1,1 +1,22 @@
 
+/*
+Program 101: Basic Selection & Text Content Modification
+Task: Select a heading element using document.getElementById() and change its text content dynamically.
+Syntax Focus: document.getElementById(), element.textContent.
+
+*/
+
+let dynBox = document.getElementById("para");
+let inp = document.getElementById("input");
+if (dynBox && dynBox.innerText.trim() === "") {
+  dynBox.innerText = "Hello I am a Dynamic Content Box......";
+}
+if (inp) {
+  inp.addEventListener("input", (event) => {
+    if (inp.value.trim() !== "") {
+      dynBox.innerText = inp.value;
+    } else {
+      dynBox.innerText = "Hello I am a Dynamic Content Box......";
+    }
+  });
+}
