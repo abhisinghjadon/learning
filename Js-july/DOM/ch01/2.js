ef2b6ac5fc7c5d2a71f59c0d24af61aa52009632
Abhishek kumar <abhisheksinghjadon663@gmail.com>
2026-08-20 21:57:19 +0530

    Update 2.js

--- Js-july/DOM/ch01/2.js
+++ Js-july/DOM/ch01/2.js
@@ -1,11 +1,4 @@
 
-/*
-Program 101: Basic Selection & Text Content Modification
-Task: Select a heading element using document.getElementById() and change its text content dynamically.
-Syntax Focus: document.getElementById(), element.textContent.
-
-*/
-
 let dynBox = document.getElementById("para");
 let inp = document.getElementById("input");
 if (dynBox && dynBox.innerText.trim() === "") {
